99bd5da15c76d2c9d5b0899c01e6217506bbbfcb
Mingyang Li <tslmy@outlook.com>
2016-08-14 17:29:41 -0400

    Installed to-markdown

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 (function($) {
-	var text = $('article').text();
-	var words = _.words(text);
-	_.forEach(words, function(word) {
+	$('span').each(function(index) {
+		var word = $(this).text();
 		console.log(word);
+		$.ajax({
+			type: "GET",
+			url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=Jimi_Hendrix&callback=?",
+			contentType: "application/json; charset=utf-8",
+			async: false,
+			dataType: "json",
+			success: function (data, textStatus, jqXHR) {
+	 			console.log(data.parse.text["*"]);
+			},
+			error: function (errorMessage) {
+			}
+		});
+
 	});
 }(jQuery));
